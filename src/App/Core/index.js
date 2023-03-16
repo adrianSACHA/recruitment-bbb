1a01cfdcb8f02f60e8bba8266da4af131f4b7ca5
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-03-16 15:31:07 +0100

    new component

---
+++ src/App/Core/index.js
@@ -0,0 +1,30 @@
+import {Navigation} from "../common/Navigation";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
+import {toCreate, toList, toUpdate} from "./routes";
+import List from "../features/List";
+import Create from "../features/Create";
+import Update from "../features/Update";
+import {theme} from "./theme";
+import {Normalize} from "styled-normalize";
+import {GlobalStyle} from "./GlobalStyle";
+import {ThemeProvider} from "styled-components";
+
+function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Normalize/>
+            <GlobalStyle/>
+            <Router>
+                <Navigation/>
+                <Routes>
+                    <Route path={toList} element={<List/>}/>
+                    <Route path={toCreate} element={<Create/>}/>
+                    <Route path={toUpdate} element={<Update/>}/>
+                    <Route path="/" element={<Navigate replace to={toList}/>}/>
+                </Routes>
+            </Router>
+        </ThemeProvider>
+    );
+}
+
+export default App;
