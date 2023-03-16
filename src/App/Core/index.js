0f7878e3c40873e9e0ae612bcfeb34920b631586
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-03-16 23:04:59 +0100

    fixed bug not showing list on landing page

--- src/App/Core/index.js
+++ src/App/Core/index.js
@@ -14,7 +14,7 @@ function App() {
         <ThemeProvider theme={theme}>
             <Normalize/>
             <GlobalStyle/>
-            <Router>
+            <Router basename="/recruitment-bbb">
                 <Navigation/>
                 <Routes>
                     <Route path={toList} element={<List/>}/>
