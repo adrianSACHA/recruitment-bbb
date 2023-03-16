a555da6ea4f6e5bd2e6c0cbe217745be46d25ed1
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-03-16 15:34:50 +0100

    styling changes

---
+++ src/App/features/Create/styled.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+  margin: 5px;
+`;
+
+export const Form = styled.form`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+`;
+
+export const Input = styled.input`
+  padding: 5px;
+  margin: 10px;
+  max-width: 200px;
+  border-radius: 5px;
+
+  &:required {
+    border-color: red;
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  grid-column: span 2;
+  display: flex;
+  justify-content: center;
+`;
