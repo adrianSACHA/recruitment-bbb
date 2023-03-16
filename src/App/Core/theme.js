d1241a340658ddae4c3261f0e3e6a385881806a4
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-03-16 15:35:47 +0100

    added theme

---
+++ src/App/Core/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+    color: {
+        mainText: "#252525",
+        slateGray: "#6e7e91",
+        dividerBackground: "#d1d5da4d",
+        mainBackgroundColor: "#FBFBFE",
+        white: "#FFFFFF",
+        buttonBackground: "#0366D6",
+        buttonText: "#FFFFFF",
+        shadow: "#090A3308",
+        borderOnHover: "#0366D633",
+        switcherBackground: "#e5e5e5",
+    },
+}
