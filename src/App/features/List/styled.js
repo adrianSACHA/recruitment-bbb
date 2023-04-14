5778d668a6393a777117f728d022f62f1f8089e6
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-04-14 11:07:25 +0200

    styled loading and error components

--- src/App/features/List/styled.js
+++ src/App/features/List/styled.js
@@ -1,5 +1,6 @@
-import styled, {css} from "styled-components";
-import {Link} from "react-router-dom";
+import styled, { css, keyframes } from "styled-components";
+import { Link } from "react-router-dom";
+import { ReactComponent as Elipse } from "./elipse.svg";
 
 export const StyledList = styled.ul`
   list-style: none;
@@ -18,22 +19,22 @@ export const StyledListItem = styled.li`
 
   &:hover,
   &:nth-child(0n) {
-    background-color: ${({theme}) => theme.color.listOnHover};
+    background-color: ${({ theme }) => theme.color.listOnHover};
   }
 
-  ${({header}) =>
-          header &&
-          css`
-            font-weight: bold;
-            font-size: 16px;
-            margin: 5px;
-          `} 
-  
-  @media (max-width: ${({theme}) => theme.breakpoint.s}px) {
+  ${({ header }) =>
+    header &&
+    css`
+      font-weight: bold;
+      font-size: 16px;
+      margin: 5px;
+    `}
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.s}px) {
     display: grid;
     grid-template-columns: 0.5fr 1fr 1fr;
     grid-auto-flow: dense;
-}
+  }
 `;
 
 export const StyledLink = styled(Link)`
@@ -47,9 +48,32 @@ export const StyledDiv = styled.div`
 export const StyledButtons = styled.div`
   display: flex;
   gap: 10px;
-  @media (max-width: ${({theme}) => theme.breakpoint.s}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoint.s}px) {
     grid-column: span 3;
     display: flex;
     justify-content: space-between;
   }
+`;
+
+const rotation = keyframes`
+ to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Loading = styled(Elipse)`
+  display: flex;
+  margin: 20vh auto;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotation} 1.3s linear infinite;
+`;
+
+export const ErrorMessage = styled.div`
+  font-size: x-large;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 20vh;
 `;
